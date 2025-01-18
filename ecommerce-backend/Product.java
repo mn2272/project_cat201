e87643f5d3cab62c5b9346f7971197bf876ed080
mn2272 <nayeem220702@gmail.com>
2025-01-18 17:55:04 +0800

    backend files

---
+++ ecommerce-backend/Product.java
@@ -0,0 +1,9 @@
+public class Product {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private String imageUrl;
+
+    // Getters and Setters
+}
