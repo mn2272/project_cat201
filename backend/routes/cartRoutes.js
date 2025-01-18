7d77db097435e3126eeb15fa28323171a6c0f172
mn2272 <nayeem220702@gmail.com>
2025-01-19 01:14:03 +0800

    reupload react js and toastify

--- backend/routes/cartRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getCart, addToCart } = require('../controllers/cartController');
-const router = express.Router();
-
-router.get('/:userId', getCart);
-router.post('/add', addToCart);
-
-module.exports = router;
