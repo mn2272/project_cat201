2bbc018a9bcd395196d683e957c8535948f4e2ba
mn2272 <nayeem220702@gmail.com>
2025-01-19 10:55:36 +0800

    added BestSeller.jsx, LatestCollection.jsx, NewsletterBox.jsx, OurPolicy,jsx and update Home.jsx

---
+++ frontend/src/components/NewsletterBox.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const NewsletterBox = () => {
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault();
+
+    }
+  return (
+    <div className='text-center'>
+        <p className='text-2xl font-medium text-gray-800'>Subcribe now & get 20% off or just die</p>
+        <p className='text-gray-400 mt-3'>
+            Dummy text
+        </p>
+        <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
+            <input className='w-full sm:flex-1 outline-none' type="email" placeholder='Please Enter your Email' required/>
+            <button type='submit' className='bg-black text-white text-xs px-10 py-4'>SUBACRIBEEE</button>
+        </form>
+      
+    </div>
+  )
+}
+
+export default NewsletterBox
