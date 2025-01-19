30ff39496019c55e2407c050dd3738c27cb18e78
mn2272 <nayeem220702@gmail.com>
2025-01-19 17:59:36 +0800

    Added place order page

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from '../components/Title';
 
 const Orders = () => {
+
+    const {products, currency} = useContext(ShopContext);
+
   return (
-    <div>
-      
+    <div className='border-t pt-16'>
+        
+        <div className='text-2xl'>
+        <Title text1={'MY'} text2={'ORDERS'}/>
+        </div>
     </div>
   )
 }
